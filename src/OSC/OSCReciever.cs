1207fe756044ee6f009fc54b1ebecf7f85751fee
ushiostarfish <ushiostarfish@gmail.com>
2016-12-06 17:55:45 +0900

    add: simple reciever class

---
+++ src/OSC/OSCReciever.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using UnityOSC;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class OSCReciever
+{
+    Queue<OSCMessage> _queue = new Queue<OSCMessage>();
+    OSCServer _server;
+
+    public void Open(int port)
+    {
+#if UNITY_EDITOR
+        if(PlayerSettings.runInBackground == false)
+        {
+            Debug.LogWarning("Recommend PlayerSettings > runInBackground = true");
+        } 
+#endif
+        if (_server != null)
+        {
+            _server.Close();
+        }
+        _server = new OSCServer(port);
+        _server.SleepMilliseconds = 0;
+        _server.PacketReceivedEvent += didRecievedEvent;
+    }
+    public void Close()
+    {
+        if (_server != null)
+        {
+            _server.Close();
+            _server = null;
+        }
+    }
+    void didRecievedEvent(OSCServer sender, OSCPacket packet)
+    {
+        lock (_queue)
+        {
+            if (packet.IsBundle())
+            {
+                var bundle = packet as OSCBundle;
+                if (0 < bundle.Data.Count)
+                {
+                    var m = bundle.Data[0] as OSCMessage;
+                    _queue.Enqueue(m);
+                }
+            }
+            else
+            {
+                _queue.Enqueue(packet as OSCMessage);
+            }
+        }
+    }
+    public bool hasWaitingMessages()
+    {
+        lock (_queue)
+        {
+            return 0 < _queue.Count;
+        }
+    }
+    public OSCMessage getNextMessage()
+    {
+        lock (_queue)
+        {
+            return _queue.Dequeue();
+        }
+    }
+}
