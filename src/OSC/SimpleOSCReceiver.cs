4a530da391fde93e384211733f54cedc5fb6843b
David Johnson <fortjay81@gmail.com>
2017-02-13 23:54:52 -0800

    started Simple OSC Receiver

---
+++ src/OSC/SimpleOSCReceiver.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityOSC;
+
+namespace UnityOSC{
+	public class SimpleOSCReceiver : MonoBehaviour {
+
+		public delegate void OscCallback(OSCMessage messsage);
+
+		private OSCHandler oscHandler;
+
+		public SimpleOSCReceiver(string name, int port){
+			oscHandler = OSCHandler.Instance;
+			oscHandler.CreateServer (name, port);
+		}
+
+		public void OnMessageReceived(string oscAddress, OscCallback function)
+		{
+			// WHAT TO DO HERE????
+		}
+	}
+}
