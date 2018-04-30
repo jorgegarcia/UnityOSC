1d7342e02f082d503a504c1195e235ab381d325c
Felix Lange <felix.lange@ixds.com>
2018-04-30 13:33:23 +0200

    added license and namespace

--- src/OSC/OSCReciever.cs
+++ src/OSC/OSCReciever.cs
@@ -1,75 +1,99 @@
-﻿using System.Collections.Generic;
+﻿//
+//	  UnityOSC - Open Sound Control interface for the Unity3d game engine
+//
+//	  Copyright (c) 2012 Jorge Garcia Martin
+//	  Last edit: Gerard Llorach 2nd August 2017
+//
+// 	  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+// 	  documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+// 	  the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+// 	  and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// 	  The above copyright notice and this permission notice shall be included in all copies or substantial portions 
+// 	  of the Software.
+//
+// 	  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+// 	  TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// 	  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+// 	  CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// 	  IN THE SOFTWARE.
+//
+
+
+using System.Collections.Generic;
 using UnityEngine;
-using UnityOSC;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
 
-public class OSCReciever
+namespace UnityOSC
 {
-    Queue<OSCMessage> _queue = new Queue<OSCMessage>();
-    OSCServer _server;
+    public class OSCReciever
+    {
+        Queue<OSCMessage> _queue = new Queue<OSCMessage>();
+        OSCServer _server;
 
-    public void Open(int port)
-    {
+        public void Open(int port)
+        {
 #if UNITY_EDITOR
-        if(PlayerSettings.runInBackground == false)
-        {
-            Debug.LogWarning("Recommend PlayerSettings > runInBackground = true");
-        } 
-#endif
-        if (_server != null)
-        {
-            _server.Close();
-        }
-        _server = new OSCServer(port);
-        _server.SleepMilliseconds = 0;
-        _server.PacketReceivedEvent += didRecievedEvent;
-    }
-    
-    public void Close()
-    {
-        if (_server != null)
-        {
-            _server.Close();
-            _server = null;
-        }
-    }
-    
-    void didRecievedEvent(OSCServer sender, OSCPacket packet)
-    {
-        lock (_queue)
-        {
-            if (packet.IsBundle())
+            if(PlayerSettings.runInBackground == false)
             {
-                var bundle = packet as OSCBundle;
-                
-                foreach(object obj in bundle.Data)
+                Debug.LogWarning("Recommend PlayerSettings > runInBackground = true");
+            } 
+#endif
+            if (_server != null)
+            {
+                _server.Close();
+            }
+            _server = new OSCServer(port);
+            _server.SleepMilliseconds = 0;
+            _server.PacketReceivedEvent += didRecievedEvent;
+        }
+        
+        public void Close()
+        {
+            if (_server != null)
+            {
+                _server.Close();
+                _server = null;
+            }
+        }
+        
+        void didRecievedEvent(OSCServer sender, OSCPacket packet)
+        {
+            lock (_queue)
+            {
+                if (packet.IsBundle())
                 {
-                    OSCMessage msg = obj as OSCMessage;
-                    _queue.Enqueue(msg);
+                    var bundle = packet as OSCBundle;
+                    
+                    foreach(object obj in bundle.Data)
+                    {
+                        OSCMessage msg = obj as OSCMessage;
+                        _queue.Enqueue(msg);
+                    }
+                }
+                else
+                {
+                    _queue.Enqueue(packet as OSCMessage);
                 }
             }
-            else
+        }
+        
+        public bool hasWaitingMessages()
+        {
+            lock (_queue)
             {
-                _queue.Enqueue(packet as OSCMessage);
+                return 0 < _queue.Count;
             }
         }
-    }
-    
-    public bool hasWaitingMessages()
-    {
-        lock (_queue)
+        
+        public OSCMessage getNextMessage()
         {
-            return 0 < _queue.Count;
-        }
-    }
-    
-    public OSCMessage getNextMessage()
-    {
-        lock (_queue)
-        {
-            return _queue.Dequeue();
+            lock (_queue)
+            {
+                return _queue.Dequeue();
+            }
         }
     }
 }
